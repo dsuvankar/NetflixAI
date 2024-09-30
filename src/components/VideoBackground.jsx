2d60b08ce6e21ae9edff8083496be13f5c1381fb
Suvankar Das <iamsuvankar25@gmail.com>
2024-09-30 20:06:07 +0530

    "removed some comments"

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -8,12 +8,8 @@ const VideoBackground = ({ movieID }) => {
 
   return (
     <div className="relative h-full w-full max-h-[1000px] overflow-hidden">
-      {/* Iframe container with gradient */}
       <div className="relative w-full h-full scale-150 aspect-video">
-        {/* Gradient applied to the iframe container */}
         <div className="absolute inset-0 bg-gradient-to-r from-gray-950 to-transparent z-10"></div>
-
-        {/* Iframe for video */}
         <iframe
           onClick={(e) => e.preventDefault()}
           className="absolute inset-0 w-full z-0  aspect-video "
